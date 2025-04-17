ba7c1449a2228265a47fdf35e10f0c1fb0ad5d16
Manuel Zuniga <manuel.z@getjobber.com>
2025-04-16 21:17:26 -0600

    add odd or even question in javascript

---
+++ javascript/odd_or_even.js
@@ -0,0 +1,11 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+//
+function evenOrOdd(number) {
+  return number % 2 ? "Even" : "Odd";
+
+  // if (number % 2) {
+  //   return "Even";
+  // } else {
+  //   return "Odd";
+  // }
+}
